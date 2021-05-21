0602e963420fa709450ba1ec15f817153cf5b0c6
Edita Keškić <edita.keskic@gmail.com>
2021-05-21 22:19:57 +0200

    Zadatak 2 opis

---
+++ sql-zadatak-2.sql
@@ -0,0 +1,25 @@
+# ZADATAK 2
+
+# 0. Kreirajte tablice (16) i veze između tablica. (6)
+
+# 1. U tablice neprijatelj, cura i decko_zarucnica unesite po 3 retka. (7)
+
+# 2. U tablici prijatelj postavite svim zapisima kolonu treciputa na 
+# vrijednost 30. travnja 2020. (4)
+
+# 3. U tablici brat obrišite sve zapise čija je vrijednost kolone ogrlica 
+# različito od 14. (4)
+
+# 4. Izlistajte suknja iz tablice cura uz uvjet da vrijednost kolone 
+# drugiputa nepoznate. (6)
+
+# 5. Prikažite novcica iz tablice zarucnica, neprijatelj iz tablice brat te 
+# haljina iz tablice neprijatelj uz uvjet da su vrijednosti kolone 
+# drugi puta iz tablice cura poznate te da su vrijednosti kolone vesta iz 
+# tablice decko sadrže niz znakova ba. Podatke posložite po haljina iz 
+# tablice neprijatelj silazno. (10)
+
+# 6. Prikažite kolone vesta i asocijalno iz tablice decko čiji se primarni 
+# ključ ne nalaze u tablici decko_zarucnica. (5)
+
+# ---------------------------------------------------------------------------------------
