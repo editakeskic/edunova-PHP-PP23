144aa8e6d468eccbdaad21ddfb888a8d78c00dbb
Edita Keškić <edita.keskic@gmail.com>
2021-05-21 22:22:09 +0200

    Update opisa

--- sql-zadatak-1.sql
+++ sql-zadatak-1.sql
@@ -1,15 +1,25 @@
 # ZADATAK 1
 
 # 0. Kreirajte tablice (16) i veze između tablica. (6)
+
 # 1. U tablice muskarac, zena i sestra_svekar unesite po 3 retka. (7)
-# 2. U tablici cura postavite svim zapisima kolonu gustoca na vrijednost 15,77. (4)
-# 3. U tablici mladic obrišite sve zapise čija je vrijednost kolone kuna veće od 15,78. (4)
-# 4. Izlistajte kratkamajica iz tablice zena uz uvjet da vrijednost kolone hlace sadrže slova ana. (6)
+
+# 2. U tablici cura postavite svim zapisima kolonu gustoca 
+# na vrijednost 15,77. (4)
+
+# 3. U tablici mladic obrišite sve zapise čija je vrijednost kolone kuna 
+# veće od 15,78. (4)
+
+# 4. Izlistajte kratkamajica iz tablice zena uz uvjet da vrijednost kolone 
+# hlace sadrže slova ana. (6)
+
 # 5. Prikažite dukserica iz tablice svekar, asocijalno iz tablice mladic te 
 # hlace iz tablice muskarac uz uvjet da su vrijednosti kolone hlace iz 
 # tablice zena počinju slovom a te da su vrijednosti kolone haljina iz 
 # tablice sestra sadrže niz znakova ba. Podatke posložite po hlace iz 
 # tablice muskarac silazno. (10)
+
 # 6. Prikažite kolone haljina i maraka iz tablice sestra čiji se primarni 
 # ključ ne nalaze u tablici sestra_svekar. (5)
 
+# ---------------------------------------------------------------------------------------
