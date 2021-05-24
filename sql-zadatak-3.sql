7bcd821fe2d6006be95c03e44fefed0111a497cb
Edita Keškić <edita.keskic@gmail.com>
2021-05-24 15:35:42 +0200

    Zadatak 3 opis

---
+++ sql-zadatak-3.sql
@@ -0,0 +1,25 @@
+# ZADATAK 
+
+# 0. Kreirajte tablice (16) i veze između tablica. (6)
+
+# 1. U tablice snasa, ostavljena i prijatelj_brat unesite po 3 retka. (7)
+
+# 2. U tablici svekar postavite svim zapisima kolonu suknja na 
+# vrijednost Osijek. (4)
+
+# 3. U tablici punica obrišite sve zapise čija je vrijednost kolone 
+# kratkamajica jednako AB. (4)
+
+# 4. Izlistajte majica iz tablice ostavljena uz uvjet da vrijednost kolone 
+# lipa nije 9,10,20,30 ili 35. (6)
+
+# 5. Prikažite ekstroventno iz tablice brat, vesta iz tablice punica te 
+# kuna iz tablice snasa uz uvjet da su vrijednosti kolone lipa iz tablice 
+# ostavljena različito od 91 te da su vrijednosti kolone haljina iz tablice 
+# prijatelj sadrže niz znakova ba. Podatke posložite po kuna iz tablice 
+# snasa silazno. (10)
+
+# 6. Prikažite kolone haljina i lipa iz tablice prijatelj čiji se primarni ključ 
+# ne nalaze u tablici prijatelj_brat. (5)
+
+# ---------------------------------------------------------------------------------------
