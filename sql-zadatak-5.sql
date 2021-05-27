a5dfb546fe23d8b2571a5a4be35585821921d8dd
Edita Keškić <edita.keskic@gmail.com>
2021-05-27 11:17:13 +0200

    Zadatak 5 opis

---
+++ sql-zadatak-5.sql
@@ -0,0 +1,26 @@
+# ZADATAK 5
+
+# 0. Kreirajte tablice (16) i veze između tablica. (6)
+
+# 1. U tablice punica, punac i svekar_cura unesite po 3 retka. (7)
+
+# 2. U tablici mladic postavite svim zapisima kolonu haljina na 
+# vrijednost Osijek. (4)
+
+# 3. U tablici ostavljena obrišite sve zapise čija je vrijednost kolone 
+# ogrlica jednako 17. (4)
+
+# 4. Izlistajte majica iz tablice punac uz uvjet da vrijednost kolone 
+# prviputa nepoznate. (6)
+
+# 5. Prikažite asocijalno iz tablice cura, stilfrizura iz tablice ostavljena te 
+# nausnica iz tablice punica uz uvjet da su vrijednosti kolone prviputa iz 
+# tablice punac poznate te da su vrijednosti kolone majica iz tablice 
+# svekar sadrže niz znakova ba. Podatke posložite po nausnica iz tablice 
+# punica silazno. (10)
+
+# 6. Prikažite kolone majica i carape iz tablice svekar čiji se primarni 
+# ključ ne nalaze u tablici svekar_cura. (5)
+
+# ---------------------------------------------------------------------------------------
+
