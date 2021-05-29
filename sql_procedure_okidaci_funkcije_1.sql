19dd87b2925d3ea14ab4772b486f573580992c0b
Edita Keškić <edita.keskic@gmail.com>
2021-05-29 09:09:27 +0200

    Primjer 1 opis

---
+++ sql_procedure_okidaci_funkcije_1.sql
@@ -0,0 +1,25 @@
+# PRIMJER 1
+
+# sql_procedure_okidaci_funkcije
+
+# 0. Kreirajte tablice (10) i veze između tablica. (3)
+# 1. Kreirati funkciju zadatak1 koja osigurava kako cjelobrojni 
+# tipovi podataka moraju biti između 980 i 5098 
+# (7). Primjeniti funkciju u minimalno jednom upitu u proceduri ili okidaču (7). 
+
+# 2. Kreirajte proceduru zadatak2 koja unosi 56872 zapisa u tablicu zarucnica (7). 
+# Izvesti proceduru jednom tako da u tablici bude točno 56872 zapisa (7). 
+
+# 3. Kreirajte okidač zadatak3 nakon insert-a u tablicu zarucnica tako da za 
+# svaki unos u tablicu zarucnica napravi po dva unosa u tablicu punac (7). 
+# Okidač je u funkciji, tablica punac ima dvostruko više zapisa od 
+# tablice zarucnica (7).
+
+# 4. Kreirajte proceduru zadatak4 koja iz tablice zarucnica 
+# zbraja svaku 7 vrijednost id-a (1,7,14,...). U tablicu 
+# muskarac se unosi broj zapisa koji odgovaraju izračunatoj sumi (7). 
+# Izvesti proceduru jednom tako da u tablici muskarac bude točan 
+# broj zapisa koji odgovaraju sumi odabranih brojeva (8).
+
+# ---------------------------------------------------------------------------------------
+
