566b620da6af450667de7c384b5e4b7e0a9ce8a4
Edita Keškić <edita.keskic@gmail.com>
2021-05-30 13:49:45 +0200

    Zadatak 6 opis

---
+++ sql-vjezbe/sql_zadatak_6.sql
@@ -0,0 +1,25 @@
+# ZADATAK 6
+
+# 0. Kreirajte tablice (16) i veze između tablica. (6)
+
+# 1. U tablice zena, brat i prijatelj_ostavljena unesite po 3 retka. (7)
+
+# 2. U tablici svekrva postavite svim zapisima kolonu suknja na 
+# vrijednost Osijek. (4)
+
+# 3. U tablici decko obrišite sve zapise čija je vrijednost kolone 
+# modelnaocala manje od AB. (4)
+
+# 4. Izlistajte narukvica iz tablice brat uz uvjet da vrijednost kolone 
+# treciputa nepoznate. (6)
+
+# 5. Prikažite drugiputa iz tablice ostavljena, zena iz tablice decko te 
+# narukvica iz tablice zena uz uvjet da su vrijednosti kolone treciputa iz 
+# tablice brat poznate te da su vrijednosti kolone prstena iz tablice 
+# prijatelj jednake broju 219. Podatke posložite po narukvica iz tablice 
+# zena silazno. (10)
+
+# 6. Prikažite kolone prstena i introvertno iz tablice prijatelj čiji se 
+# primarni ključ ne nalaze u tablici prijatelj_ostavljena. (5)
+
+# ---------------------------------------------------------------------------------------
