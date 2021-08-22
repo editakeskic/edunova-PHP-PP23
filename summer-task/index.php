284fe860c846be2aadeb389391c6f8f71d6717e1
Edita Keškić <edita.keskic@gmail.com>
2021-08-22 22:55:41 +0200

    Created basic index page

---
+++ summer-task/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>yogastudio.xyz</title>
+</head>
+<body>
+    <form action="authorization.php" method="post">
+    
+    <label for="email">Email</label>
+    <input type="text" name="email" id="email">
+
+    <label for="lozinka">Password</label>
+    <input type="password" name="password" id="password">
+
+    <input type="submit" value="Log in">
+    
+    </form>
+</body>
+</html>
