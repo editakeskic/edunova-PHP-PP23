99a4f62c5483f5cdbe65f12473d759a55541f859
Edita Keškić <edita.keskic@gmail.com>
2021-08-22 22:55:28 +0200

    Created basic authorization page

---
+++ summer-task/authorization.php
@@ -0,0 +1,9 @@
+<?php
+
+if($_POST['email']==='user1@email.com' && $_POST['password']==='enter'){
+    session_start();
+    $_SESSION['authorized']=$_POST['email'];
+    header('location: protectedPage.php');
+}else{
+    header('location: index.php');
+}
